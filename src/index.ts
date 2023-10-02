57144523cd23d05ff4211e1a5ddbb94f4da1dc65
Greg Kimmy <greg.kimmy@gmail.com>
2023-10-01 21:36:27 -0500

    fix: improve type safety

--- src/index.ts
+++ src/index.ts
@@ -212,87 +212,78 @@ export class UnknownError extends Error {
 }
 
 export const socketProtocolErrorStatuses: { [code: number]: string } = {
-  1001: 'Socket was disconnected',
-  1002: 'A WebSocket protocol error was encountered',
-  1003: 'Server terminated socket because it received invalid data',
-  1005: 'Socket closed without status code',
-  1006: 'Socket hung up',
-  1007: 'Message format was incorrect',
-  1008: 'Encountered a policy violation',
-  1009: 'Message was too big to process',
-  1010: 'Client ended the connection because the server did not comply with extension requirements',
-  1011: 'Server encountered an unexpected fatal condition',
-  4000: 'Server ping timed out',
-  4001: 'Client pong timed out',
-  4002: 'Server failed to sign auth token',
-  4003: 'Failed to complete handshake',
-  4004: 'Client failed to save auth token',
-  4005: 'Did not receive #handshake from client before timeout',
-  4006: 'Failed to bind socket to message broker',
-  4007: 'Client connection establishment timed out',
-  4008: 'Server rejected handshake from client',
-  4009: 'Server received a message before the client handshake'
+	1001: 'Socket was disconnected',
+	1002: 'A WebSocket protocol error was encountered',
+	1003: 'Server terminated socket because it received invalid data',
+	1005: 'Socket closed without status code',
+	1006: 'Socket hung up',
+	1007: 'Message format was incorrect',
+	1008: 'Encountered a policy violation',
+	1009: 'Message was too big to process',
+	1010: 'Client ended the connection because the server did not comply with extension requirements',
+	1011: 'Server encountered an unexpected fatal condition',
+	4000: 'Server ping timed out',
+	4001: 'Client pong timed out',
+	4002: 'Server failed to sign auth token',
+	4003: 'Failed to complete handshake',
+	4004: 'Client failed to save auth token',
+	4005: 'Did not receive #handshake from client before timeout',
+	4006: 'Failed to bind socket to message broker',
+	4007: 'Client connection establishment timed out',
+	4008: 'Server rejected handshake from client',
+	4009: 'Server received a message before the client handshake'
 };
 
 export const socketProtocolIgnoreStatuses: { [code: number]: string } = {
-  1000: 'Socket closed normally',
-  1001: 'Socket hung up'
-};
-
-// Properties related to error domains cannot be serialized.
-const unserializableErrorProperties: { [key: string]: 1 } = {
-  domain: 1,
-  domainEmitter: 1,
-  domainThrown: 1
+	1000: 'Socket closed normally',
+	1001: 'Socket hung up'
 };
 
 export type DehydratedError = any;
 
 // Convert an error into a JSON-compatible type which can later be hydrated
 // back to its *original* form.
-export function dehydrateError(error: any, includeStackTrace?: boolean): DehydratedError {
-  let dehydratedError: any;
+export function dehydrateError(error: any): DehydratedError {
+	let dehydratedError: any;
 
-  if (error && typeof error === 'object') {
-    dehydratedError = {
-      message: error.message
-    };
+	if (error && typeof error === 'object') {
+		dehydratedError = {
+			message: error.message
+		};
 
-    if (includeStackTrace) {
-      dehydratedError.stack = error.stack;
-    }
+		for (let i of Object.keys(error)) {
+			dehydratedError[i] = error[i];
+		}
+	} else if (typeof error === 'function') {
+		dehydratedError = '[function ' + (typeof error.name === 'string' ? error.name : 'anonymous') + ']';
+	} else {
+		dehydratedError = error;
+	}
 
-    for (var i in error) {
-      if (!unserializableErrorProperties[i]) {
-        dehydratedError[i] = error[i];
-      }
-    }
-  } else if (typeof error === 'function') {
-    dehydratedError = '[function ' + (error.name || 'anonymous') + ']';
-  } else {
-    dehydratedError = error;
-  }
-
-  return decycle(dehydratedError);
-};
+	return decycle(dehydratedError);
+}
 
 // Convert a dehydrated error back to its *original* form.
 export function hydrateError(error: DehydratedError): any {
-  var hydratedError = null;
+	let hydratedError: { [ key: string ] : any } = null;
 
-  if (error != null) {
-    if (typeof error === 'object') {
-      hydratedError = new Error(error.message);
+	if (error != null) {
+		if (typeof error === 'object') {
+			hydratedError = new Error(typeof error.message === 'string' ? error.message : 'Invalid error message format');
 
-      for (var i in error) {
-        if (error.hasOwnProperty(i)) {
-          hydratedError[i as keyof Error] = error[i];
-        }
-      }
-    } else {
-      hydratedError = error;
-    }
-  }
+			if (typeof error.name === 'string') {
+				hydratedError.name = error.name;
+			}
 
-  return hydratedError;
-};
+			for (let i of Object.keys(error)) {
+				if (hydratedError[i] === undefined) {
+					hydratedError[i as keyof Error] = error[i];
+				}
+			}
+		} else {
+			hydratedError = error;
+		}
+	}
+
+	return hydratedError;
+}
